b3a3c0858483153280586e75393a35318a0fae57
Bryan Mateer <email": null>
2017-05-04 12:30:37 -0400

    Add routes for login/logout

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -16,3 +16,19 @@ end
 get '/users/login' do
   erb :'users/login'
 end
+
+post '/users/login' do
+  user = User.find_by(username: params[:username])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    @errors = ["Username or Password is invalid"]
+    erb :'users/login'
+  end
+end
+
+get '/users/logout' do
+  session.clear
+  redirect '/'
+end
