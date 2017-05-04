a9bfbd8cc840b2788ad55cb9fc4067edab1ddf7b
Bryan Mateer <email": null>
2017-05-04 16:18:22 -0400

    Protect routes for searching for recipes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,6 +3,7 @@ get '/' do
 end
 
 get '/search' do
+  require_user
   query = params[:ingredients].to_query('')
   uri = URI.parse("http://food2fork.com/api/search?key=#{ENV['ACCESS_TOKEN']}&q#{query}")
   response = Net::HTTP.get_response(uri)
@@ -11,6 +12,7 @@ get '/search' do
 end
 
 get '/recipes/:recipe_id' do
+  require_user
   uri = URI.parse("http://food2fork.com/api/get?key=#{ENV['ACCESS_TOKEN']}&rId=#{params[:recipe_id]}")
   response = Net::HTTP.get_response(uri)
   @recipe = JSON.parse(response.body)['recipe']
