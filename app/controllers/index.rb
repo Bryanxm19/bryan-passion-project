3ea022a2bb6b2e82745c7d2fce7cb54a76419722
Bryan Mateer <email": null>
2017-05-04 16:14:02 -0400

    Add routes for recipe search and a specific recipe

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,18 @@
 get '/' do
   erb :index
 end
+
+get '/search' do
+  query = params[:ingredients].to_query('')
+  uri = URI.parse("http://food2fork.com/api/search?key=#{ENV['ACCESS_TOKEN']}&q#{query}")
+  response = Net::HTTP.get_response(uri)
+  @body = JSON.parse(response.body)
+  erb :search
+end
+
+get '/recipes/:recipe_id' do
+  uri = URI.parse("http://food2fork.com/api/get?key=#{ENV['ACCESS_TOKEN']}&rId=#{params[:recipe_id]}")
+  response = Net::HTTP.get_response(uri)
+  @recipe = JSON.parse(response.body)['recipe']
+  erb :recipe
+end
