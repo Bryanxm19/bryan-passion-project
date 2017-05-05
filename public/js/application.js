4d7800a48f2de5b264dc0bb32e01f2c9a2c02cc9
Bryan Mateer <email": null>
2017-05-05 15:25:33 -0400

    Add event listener for clicking on a recipe

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $(".recipes li").on("click", function(event) {
+    event.preventDefault();
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    var $recipe = this;
+    var url = $(this).find("a").attr("href");
+
+    $.ajax({
+      method: "GET",
+      url: url
+    })
+    .done(function(response) {
+      if($($recipe).hasClass("clicked")) {
+        $($recipe).removeClass("clicked");
+        $(".recipes li div#recipe-details").remove();
+      }
+      else {
+        $($recipe).addClass("clicked");
+        $($recipe).append(response);
+      }
+    })
+  })
 });
